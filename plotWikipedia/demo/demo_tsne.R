bc05e271297c995c4b4f4b3638faa8861645a33a
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2016-08-02 21:22:56 +0900

    finished

--- plotWikipedia/demo/demo_tsne.R
+++ plotWikipedia/demo/demo_tsne.R
@@ -3,5 +3,6 @@ library("Rtsne")
 data("Word2Vec")
 data("label.Pokemon")
 
+set.seed(123)
 res.tsne <- Rtsne(Word2Vec, dims=2)
 plotlyScatter(res.tsne, label=rownames(Word2Vec), color=label.Pokemon)
